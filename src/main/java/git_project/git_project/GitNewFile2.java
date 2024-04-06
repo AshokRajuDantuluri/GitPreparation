0d7739800796d3969f562b80109543f7b256f034
ashok <ashokdonthuluri999@gmail.com>
2024-04-06 12:56:45 +0530

    Committed my second file

---
+++ src/main/java/git_project/git_project/GitNewFile2.java
@@ -0,0 +1,11 @@
+package git_project.git_project;
+
+public class GitNewFile2 {
+
+	public static void main(String[] args) {
+            
+		   System.out.println("Git Second File");
+		   
+	}
+
+}
