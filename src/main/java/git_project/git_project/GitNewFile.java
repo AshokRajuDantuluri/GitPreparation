0be7e01618afc0d521e9121043d2a81f4f784eef
ashok <ashokdonthuluri999@gmail.com>
2024-04-06 12:48:14 +0530

    Commit

--- src/main/java/git_project/git_project/GitNewFile.java
+++ src/main/java/git_project/git_project/GitNewFile.java
@@ -5,6 +5,9 @@ public class GitNewFile {
 	public static void main(String[] args) {
             
 		   System.out.println("Git File");
+		   
+		   System.out.println("Git File for second commit");
+
 	}
 
 }
